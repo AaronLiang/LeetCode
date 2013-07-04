6a68a38720d34171a80a933732435552f6700744
AaronLiang <aaron.itjob@gmail.com>
2013-07-04 14:07:17 -0400

    Update and rename Add Two Numbers to Add Two Numbers.java

--- Add Two Numbers.java
+++ Add Two Numbers.java
@@ -93,3 +93,37 @@ public class Solution {
         
     }
 }
+
+
+==============================================================================================
+public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    int carry = 0;
+    ListNode rootListNode = new ListNode(-1);
+    ListNode currentListNode = rootListNode;
+
+    while (l1 != null || l2 != null) {
+        int sum = carry;
+        if (l1 != null) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        if (l2 != null) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+
+        if (sum >= 10) {
+            carry = 1;
+            sum -= 10;
+        } else {
+            carry = 0;   
+        }
+        currentListNode.next = new ListNode(sum);
+        currentListNode = currentListNode.next;
+    }
+    if (carry == 1) {
+        currentListNode.next = new ListNode(carry);
+    }
+
+    return rootListNode.next;
+}
