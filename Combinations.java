70d637c1af57cd2043373aabda2c6e45fa420905
AaronLiang <aaron.itjob@gmail.com>
2013-07-07 22:00:58 -0400

    Update Combinations.java

--- Combinations.java
+++ Combinations.java
@@ -12,3 +12,31 @@ If n = 4 and k = 2, a solution is:
   [1,3],
   [1,4],
 ]
+
+public class Solution {
+    public ArrayList<ArrayList<Integer>> combine(int n, int k) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if (n < k || k < 0) return null;
+        ArrayList<ArrayList<Integer>> lists = new ArrayList<ArrayList<Integer>>();
+        if ( k == 0 ) return lists;
+        
+        if ( k == 1){
+            for (int i = 1 ; i <= n; i++){
+                ArrayList<Integer> list = new ArrayList<Integer>();
+                list.add(i);
+                lists.add(list);
+            }
+            return lists;
+        }
+        
+        for(int i = n; i >= k; i--){
+            ArrayList<ArrayList<Integer>> currentlist = combine(i-1, k-1);
+            for(ArrayList<Integer> list : currentlist){
+                list.add(i);
+                lists.add(list);
+            }
+        }
+        return lists;
+    }
+}
