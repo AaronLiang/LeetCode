4110cb8a5db24d6f77f0b05b99cb0b3649de8897
AaronLiang <aaron.itjob@gmail.com>
2013-07-24 20:46:13 -0400

    Update Anagrams.java

--- Anagrams.java
+++ Anagrams.java
@@ -9,6 +9,33 @@ Given an array of strings, return all groups of strings that are anagrams.
 Note: All inputs will be in lower-case.
 
 
+
+public class Solution {
+    public ArrayList<String> anagrams(String[] strs) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ArrayList<String> anagram = new ArrayList<String>();
+        HashMap<String,ArrayList<String>> list = new HashMap<String,ArrayList<String>>();
+        for (String str: strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            if (list.containsKey(key)) {
+                list.get(key).add(str);
+            } else {
+                list.put(key,new ArrayList<String>(Arrays.asList(str)));
+            }
+        }
+        for (ArrayList<String> test:list.values()) {
+            if (test.size()>1) {
+                anagram.addAll(test);
+            }
+        }
+        return anagram;
+    }
+}
+=======================================================================================================
+
 public class Solution {
     public ArrayList<String> anagrams(String[] strs) {
         // Start typing your Java solution below
