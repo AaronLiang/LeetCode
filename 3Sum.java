2ef4ab361acefc15dfa9d58a351c60187382e640
AaronLiang <aaron.itjob@gmail.com>
2013-07-24 20:19:39 -0400

    Update and rename 3Sum to 3Sum.java

--- 3Sum.java
+++ 3Sum.java
@@ -3,7 +3,7 @@ Given an array S of n integers, are there elements a, b, c in
 S such that a + b + c = 0? Find all unique triplets in the 
 array which gives the sum of zero.
 
-Note:
+Note: http://discuss.leetcode.com/questions/15/3sum
 
 Elements in a triplet (a,b,c) must be in non-descending order. 
 (ie, a ? b ? c)
@@ -13,6 +13,42 @@ The solution set must not contain duplicate triplets.
     A solution set is:
     (-1, 0, 1)
     (-1, -1, 2)
+    
+    ================================================================
+    public class Solution {
+    public ArrayList<ArrayList<Integer>> threeSum(int[] num) {  
+      ArrayList<ArrayList<Integer>> resSet = new ArrayList<ArrayList<Integer>>();  
+      if (num.length < 3) return resSet;  
+      Arrays.sort(num);  
+      for (int i=0; i<num.length-2 && num[i]<=0; ++i) {  
+        // remove duplicates  
+        if (i>0 && num[i]==num[i-1]) continue;  
+        // use two pointer to find b+c = -a  
+        int start=i+1, end=num.length-1;  
+        while (start<end) {  
+          int sum = num[i] + num[start] + num[end];  
+          if (sum < 0) {  
+            start++;  
+          } else if (sum > 0) {  
+            end--;  
+          } else { // find one  
+            ArrayList<Integer> res = new ArrayList<Integer>(3);  
+            res.add(num[i]);  
+            res.add(num[start]);  
+            res.add(num[end]);  
+            resSet.add(res);  
+            // move the left pointer to right and skip duplicates;  
+            do { start++; } while (start<end && num[start]==num[start-1]);  
+            // move the right pointer to left and skip duplicates  
+            do { end--; } while (start<end && num[end]==num[end+1]);  
+          }  
+        }  
+      }  
+      return resSet;  
+    }
+}
+    ===========================================================================
+    
     
         public ArrayList<ArrayList<Integer>> threeSum(int[] num1) {
         // Start typing your Java solution below
